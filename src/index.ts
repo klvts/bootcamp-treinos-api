6e7ff87a20daf5cec0c2cbe19b9e4d0d6ac1a9b0
klvts <klevison.dev@gmail.com>
2026-07-01 21:48:28 -0300

    feat: add fastify api

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "hello world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8081 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
